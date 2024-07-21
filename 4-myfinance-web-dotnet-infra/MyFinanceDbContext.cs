80db1e0560fdaa61f5f6a50a3eb399dc604b4df6
leticiancalvess <leticiancalves1@gmail.com>
2024-07-21 13:02:40 -0300

    Entity and DB

---
+++ 4-myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace myfinance_web_dotnet_infra;
+using Microsoft.EntityFrameworkCore;
+using myfinance_web_dotnet_domain.Entities;
+
+public class MyFinanceDbContext : DbContext
+{
+    public DbSet<PlanoConta> PlanoConta { get; set; }
+    public DbSet<Transacao> Transacao { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;");
+    }
+}
--- myfinance-web-dotnet/Program.cs
+++ myfinance-web-dotnet/Program.cs
@@ -1,7 +1,13 @@
+using myfinance_web_dotnet_infra;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MyFinanceDbContext>();
 
 var app = builder.Build();
 
