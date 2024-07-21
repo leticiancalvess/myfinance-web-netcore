58284f44320d3e2b3bfc438f9591491d1b7da178
leticiancalvess <leticiancalves1@gmail.com>
2024-07-21 13:53:18 -0300

    services layer

---
+++ 2-myfinance-web-dotnet-service/PlanoContaService.cs
@@ -0,0 +1,46 @@
+using myfinance_web_dotnet_domain.Entities;
+using myfinance_web_dotnet_service.Interfaces;
+using myfinance_web_dotnet_infra;
+
+
+
+namespace myfinance_web_dotnet_service
+{
+    public class PlanoContaService : IPlanoContaService
+    {
+        private readonly MyFinanceDbContext _dbContext;
+        public PlanoContaService(MyFinanceDbContext dbContext) {
+            _dbContext = dbContext;
+
+        }
+
+        public void Cadastrar(PlanoContaService Entidade) {
+            var dbSet = _dbContext.PlanoConta;
+            if (Entidade.Id == null) {
+                dbSet.Add(Entidade);
+            }
+            else{
+                dbSet.Attach(Entidade);
+                _dbContext.Entry(Entidade).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            }
+            _dbContext.SaveChanges();
+        }
+        public void Excluir(int Id)
+        {
+            var PlanoConta = new PlanoContaService() {
+                Id = Id
+                _dbContext.Attach(PlanoConta);
+                _dbContext.Remove(PlanoConta);
+                _dbContext.SaveChanges();
+            };
+            public List<PlanoContaService> ListarRegistros() {
+                var dbSet = _dbContext.PlanoConta;
+                return dbSet.ToList();
+            }
+            public PlanoContaService RetornarRegistro(int Id){
+                return _dbContext.PlanoConta.Where(x => x.Id == Id).First();
+            }
+
+        }
+    }
+}
